01d79105bb900582f84243668a640fe8ab592ace
Ruben7ctrl <rubencabellomiguel@gmail.com>
2025-03-31 16:51:13 +0000

    first react page

--- src/js/components/footer.jsx
+++ src/js/components/footer.jsx
@@ -6,7 +6,7 @@ export const Footer = () => {
     return (
         <div className="bg-black p-5 text-center text-white">
             <div className="conteiner">
-                <p>Copyright &copy; Your Website {new Date().getFullYear()}</p>
+                <p>Copyright © Your Website {new Date().getFullYear()}</p>
             </div>
         </div>
     )
